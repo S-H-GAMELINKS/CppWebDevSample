788a898fbd110de2d66af0b9e4c1b42479f473db
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-02 22:11:20 +0900

    using C++ web dev for Doker

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include "cpp-httplib/httplib.h"
 
 int main(){
 
     std::cout << "C++ Web API is up!" << std::endl;
+
+    httplib::Server svr;
+
+    svr.Get("/", [](const httplib::Request& req, httplib::Response& res){
+        std::cout << "GET: routes '/'" << std::endl;
+        res.set_content("Hi! Welcome to C++ Web App!", "text/plain");
+    });
+
+    svr.listen("0.0.0.0", 3000);
 
     return 0;
 }
