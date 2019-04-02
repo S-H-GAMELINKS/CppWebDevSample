da93969d212ef03768aaabe4e7755f9f1443678d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-02 22:27:35 +0900

    render static/index.html

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include "cpp-httplib/httplib.h"
+
+const std::string load_static(const std::string& path) {
+
+    std::ifstream static_file(path.c_str(), std::ios::in);
+
+    std::stringstream stream;
+
+    stream << static_file.rdbuf();
+    static_file.close();
+
+    return stream.str();
+}
 
 int main(){
 
@@ -7,9 +22,11 @@ int main(){
 
     httplib::Server svr;
 
-    svr.Get("/", [](const httplib::Request& req, httplib::Response& res){
+    const std::string static_html = load_static("./static/index.html");
+
+    svr.Get("/", [&](const httplib::Request& req, httplib::Response& res){
         std::cout << "GET: routes '/'" << std::endl;
-        res.set_content("Hi! Welcome to C++ Web App!", "text/plain");
+        res.set_content(static_html.c_str(), "text/html");
     });
 
     svr.listen("0.0.0.0", 3000);
