843fe048690572852fb7b79785b64fae384f6324
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-03 22:45:33 +0900

    add /api/talks

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <array>
+#include <vector>
 #include "cpp-httplib/httplib.h"
 
 const std::string load_static(const std::string& path) {
@@ -29,6 +30,8 @@ int main(){
 
     const std::string static_javascript = load_static("./static/index.js");
 
+    std::vector<std::string> talks;
+
     for(auto&& r : routes)
         svr.Get(r.c_str(), [&](const httplib::Request& req, httplib::Response& res){
             std::cout << "GET: routes " << r << std::endl;
@@ -38,6 +41,17 @@ int main(){
     svr.Get("/index.js", [&](const httplib::Request& req, httplib::Response& res){
         std::cout << "GET: routes '/index.js'" << std::endl;
         res.set_content(static_javascript.c_str(), "text/javascript");
+    });
+
+    svr.Post("/api/talks", [&](const httplib::Request& req, httplib::Response& res) {
+        try {
+            talks.emplace_back(std::move(req.body));
+            std::cout << "Create Talks!" << std::endl;
+            res.set_content("Create Talks!", "text/plain");
+        } catch(...) {
+            std::cout << "Can not Create Talks!" << std::endl;
+            res.set_content("Can not Create Talks!", "text/plain");
+        }
     });
 
     svr.listen("0.0.0.0", 3000);
