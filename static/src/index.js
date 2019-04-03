e688297c38869c880de376e47e6996df0c8bc18f
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-03 18:36:45 +0900

    fix using bootstrap-umi

--- static/src/index.js
+++ static/src/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import * as Bootstrap from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
 
+Vue.use(Bootstrap);
+
 const app = new Vue({
     el: '.app',
     data: function() {
