13ad7b03bf264a86996b29fa003f34d42989aecb
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-03 22:14:58 +0900

    add components/web/About.vue

---
+++ static/components/web/About.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>About Pages</h1>
+        <p>This is Web Development Sample based by C++/Vue.js/Stimulus/Docker.</p>
+        <p>License is MIT.</p>
+    </div>    
+</template>
