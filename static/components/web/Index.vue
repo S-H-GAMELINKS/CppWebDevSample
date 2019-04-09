099d01f471e77facd1388bb0e7b3c2bb32eb8763
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-09 22:23:20 +0900

    fix tag

--- static/components/web/Index.vue
+++ static/components/web/Index.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Index Pages</h1>
-        <p>Let's Enjoy C++/Vue.js/Stimulus Realtime Chat!<p>
+        <p>Let's Enjoy C++/Vue.js/Stimulus Realtime Chat!</p>
         
         <router-link to="/chats">Let's! Chat!</router-link>
     </div>    
