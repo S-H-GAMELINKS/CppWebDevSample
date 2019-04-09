980b4d572b28aeea9b711266e59cd7d0290d4ba9
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-09 22:14:26 +0900

    remove chats & add link /chats

--- static/components/web/Index.vue
+++ static/components/web/Index.vue
@@ -2,18 +2,7 @@
     <div>
         <h1>Index Pages</h1>
         <p>Let's Enjoy C++/Vue.js/Stimulus Realtime Chat!<p>
-            
-        <div class="container" data-controller="chat" data-chat-url="/api/talks" data-chat-refresh-interval="100">
-            <div class="input-group">
-                <div class="input-group-append">
-                    <span class="input-group-text">Input Tlack</span>
-                </div>
-                <input type="text" class="form-control" data-target="chat.content"> 
-            </div>
-            <p>
-                <button type="button" class="btn btn-primary" data-action="click->chat#submit">Submit</button>
-            </p>
-            <div data-target="chat.talks"></div>
-        </div>
+        
+        <router-link to="/chats">Let's! Chat!</router-link>
     </div>    
 </template>
