76a21c4d8735a9deca712903fa12f1b7bbd3c992
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-09 22:23:12 +0900

    add chats/ChatRoom.vue

---
+++ static/components/chats/ChatRoom.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container" data-controller="chat" data-chat-url="/api/talks" data-chat-refresh-interval="100">
+        <div class="input-group">
+            <div class="input-group-append">
+                <span class="input-group-text">Input Tlack</span>
+            </div>
+            <input type="text" class="form-control" data-target="chat.content"> 
+        </div>
+        <p>
+            <button type="button" class="btn btn-primary" data-action="click->chat#submit">Submit</button>
+        </p>
+        <div data-target="chat.talks"></div>
+    </div>
+</template>
