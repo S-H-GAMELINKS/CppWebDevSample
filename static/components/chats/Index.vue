c428bebff91c1a0cbb528aa0d3b289211adcb2cc
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-09 22:18:10 +0900

    add chats/Index.vue

---
+++ static/components/chats/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container" data-controller="room" data-chat-url="/api/talks" data-chat-refresh-interval="100">
+        <h1>All Chat Room's</h1>
+        <div class="input-group">
+            <div class="input-group-append">
+                <span class="input-group-text">Create New Chat Room</span>
+            </div>
+            <input type="text" class="form-control" data-target="room.new"> 
+        </div>
+        <p>
+            <button type="button" class="btn btn-primary" data-action="click->room#submit">Submit</button>
+        </p>
+
+        <div data-target="rooms"></div>
+    </div>
+</template>
